9e2a1e00e33d541878d4fe2f858b02e25399c77e
originap <originap@naver.com>
2025-04-29 19:24:13 +0900

    add : 로그인 페이지 템플릿

--- front_end/src/views/LoginPage.vue
+++ front_end/src/views/LoginPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>LoginPage</div>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label>Email:</label>
+        <input v-model="email" type="email" required />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input v-model="password" type="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
