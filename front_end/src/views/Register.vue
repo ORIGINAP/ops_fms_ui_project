65df57b764b28dfc08805378f60f57dbd307dcbf
DaveLEE777 <ss98565009@gmail.com>
2025-05-20 23:12:38 +0900

    register.vue 스크립트 작성 서버로 회원정보 넘기는 용도

--- front_end/src/views/Register.vue
+++ front_end/src/views/Register.vue
@@ -3,9 +3,22 @@
     <h2>회원가입 페이지</h2> <!--css 하면서 여기도 수정해 주세요-->
     <input v-model="username" placeholder="Your ID" />
     <input v-model="password" placeholder="Your PW" />
-    <button @click="register">가입</button>
+    <button @click="SendServer">가입</button>
   </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue'
+    import axios from 'axios'
+
+    const id = ref('')
+    const pw = ref('')
+
+    const SendServer = async () => {
+        await axios.post('http://localhost:5000/register', {
+            id : id.value,
+            pw : pw.value
+        })
+    }
+
 </script>
