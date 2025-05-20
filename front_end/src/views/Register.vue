0403fa6159c69f8ea76882adae588defa53c9a5a
DaveLEE777 <ss98565009@gmail.com>
2025-05-20 23:00:32 +0900

    register.vue 파일 템플릿 작성

--- front_end/src/views/Register.vue
+++ front_end/src/views/Register.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>register page</div>
+  <div>
+    <h2>회원가입 페이지</h2> <!--css 하면서 여기도 수정해 주세요-->
+    <input v-model="username" placeholder="Your ID" />
+    <input v-model="password" placeholder="Your PW" />
+    <button @click="register">가입</button>
+  </div>
 </template>
 
 <script setup>
