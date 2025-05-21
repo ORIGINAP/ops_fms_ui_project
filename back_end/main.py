16ff6866843ffe097255445aeca75109a84e060d
DaveLEE777 <ss98565009@gmail.com>
2025-05-21 21:25:02 +0900

    main.py의 sqlite db 구축

--- back_end/main.py
+++ back_end/main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    key = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(80), unique=True, nullable=False)
+    pw = db.Column(db.String(80), nullable=False)
 
 @app.route('/')
 def home():
