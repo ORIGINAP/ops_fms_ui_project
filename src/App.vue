b95aae258090f3873b3de21d5faf0f11e6ef0df2
mountaindew93 <yangsikyeong@gmail.com>
2025-05-22 01:04:24 +0900

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- src/App.vue -->
+<template>
+  <MainPage />
+</template>
+
+<script>
+import MainPage from './views/MainPage.vue';
+
+export default {
+  components: { MainPage },
+};
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+</style>
