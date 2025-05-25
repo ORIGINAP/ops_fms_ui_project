bec9bcc72c1e9ab9b063b8d6135480f1eed327a0
DaveLEE777 <ss98565009@gmail.com>
2025-05-25 12:04:12 +0900

    회원가입 페이지 css수정

--- front_end/src/views/Register.vue
+++ front_end/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h2>회원가입 페이지</h2> <!--css 하면서 여기도 수정해 주세요-->
+  <div class="login-container"> <!--로그인 css 재사용-->
+    <h2>회원가입 페이지</h2> 
     <input v-model="username" placeholder="Your ID" />
     <input v-model="password" placeholder="Your PW" />
     <button @click="SendServer">가입</button>
@@ -39,3 +39,48 @@
     
     }
 </script>
+
+<style scoped>
+.login-container {
+  width: 300px;
+  margin: 100px auto;
+  padding: 24px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+}
+
+.login-container h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.login-container label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+.login-container input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.login-container button {
+  width: 100%;
+  padding: 10px;
+  background-color: #254081;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.login-container button:hover {
+  background-color: #1e305d;
+}
+</style>
