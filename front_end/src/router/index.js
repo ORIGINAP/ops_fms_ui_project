e96b443ce6cf5d2a789ecab4403984e3abb744c8
moutaindew93 <yangsikyeong@gmail.com>
2025-05-30 16:03:30 +0900

    페이지 생성 및 연결 설정

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/LoginPage.vue';
 import HomeView from '../views/HomeView.vue'
 import Register from '../views/Register.vue';
+import LogPage from '../views/LogPage.vue';
+import RoutePage from '../views/RoutePage.vue';
+import SystemPage from '../views/SystemPage.vue';
 
 //여기서 라우터 설정을 하시면 됩니다.
 const routes = [
-  { path: '/', name: 'Login', component: Login },
+  { path: '/', name: 'Login', component: HomeView },
   { path: '/main', name: 'home', component: HomeView },
   { path: '/register', name: 'register', component: Register },
+  { path: '/LogPage', name: 'LogPage', component: LogPage },
+  { path: '/RoutePage', name: 'RoutePage', component: RoutePage },
+  { path: '/SystemPage', name: 'SystemPage', component: SystemPage },
 ]
 
 const router = createRouter({
