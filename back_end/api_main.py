1cea187399ce71035463e4c358125632a7961052
YeongMin <162403242+ORIGINAP@users.noreply.github.com>
2025-06-05 02:16:34 +0000

    Edit: api 변경

--- back_end/api_main.py
+++ back_end/api_main.py
@@ -4,19 +4,31 @@ from flask_cors import CORS
 api = Flask(__name__)
 CORS(api)  # 모든 출처 허용
 
-@api.route('/robot', methods=['GET'])
-@api.route('/robot1', methods=['GET'])
-@api.route('/robot2', methods=['GET'])
-@api.route('/robot3', methods=['GET'])
-@api.route('/robot4', methods=['GET'])
-
-
-def robot():
+@api.route('/robotA', methods=['GET'])
+def robotA():
     robot= {
-        "name": "Robot",
+        "name": "Robot-A",
         "version": "1.0",
-        "description": "This is a robot API",
+        "velocity": "1",
+        "battery": "100%",
+        "route": "A#B",
     }
+    return output(robot)
+    
+@api.route('/robotB', methods=['GET'])
+def robotB():
+    robot= {
+        "name": "Robot-B",
+        "version": "1.0",
+        "velocity": "1",
+        "battery": "70%",
+        "route": "B#C",
+    }
+    return output(robot)
+
+
+
+def output(robot):
     field = request.args.get('field', default='name', type=str)
     if field == 'name':
         return robot['name'], 200, {'Content-Type': 'text/plain'}
@@ -28,25 +40,7 @@ def robot():
         return robot, 200, {'Content-Type': 'application/json'}
     else:   
         return {"error": "Field not found"}, 404
-    
-    
-def robot1():
-    robot= {
-        "name": "Robot",
-        "version": "1.0",
-        "description": "This is a robot API",
-    }
-    field = request.args.get('field', default='name', type=str)
-    if field == 'name':
-        return robot['name'], 200, {'Content-Type': 'text/plain'}
-    elif field == 'version':
-        return robot['version'], 200, {'Content-Type': 'text/plain'}
-    elif field == 'description':
-        return robot['description'], 200, {'Content-Type': 'text/plain'}
-    elif field == 'robot':
-        return robot, 200, {'Content-Type': 'application/json'}
-    else:   
-        return {"error": "Field not found"}, 404
+
 
 if __name__ == '__main__':
     api.run(debug=True, port=5001, host='0.0.0.0')
