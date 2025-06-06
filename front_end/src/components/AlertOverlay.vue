e1841b85ebf069161bc02bd6a2407d0811f648ab
zxczxczczz <justwidenme@gmail.com>
2025-06-06 17:38:47 +0900

    feat: AlertOverlay 기본 데이터 구현

--- front_end/src/components/AlertOverlay.vue
+++ front_end/src/components/AlertOverlay.vue
@@ -1,8 +1,22 @@
 <template>
-    <div v-if="isActive" class="alert-overlay" :class="{ 'blinking': isBlinking }">
-      <div class="alert-content">
-        <div class="alert-icon">⚠️</div>
-        <div class="alert-message">{{ message }}</div>
-      </div>
+  <div v-if="isActive" class="alert-overlay" :class="{ 'blinking': isBlinking }">
+    <div class="alert-content">
+      <div class="alert-icon">⚠️</div>
+      <div class="alert-message">{{ message }}</div>
     </div>
-  </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertOverlay',
+  data() {
+    return {
+      isActive: false,
+      isBlinking: false,
+      message: '',
+      blinkInterval: null
+    }
+  }
+}
+</script>
