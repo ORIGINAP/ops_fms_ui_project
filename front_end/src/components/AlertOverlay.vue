7a4bdc3c9c189fd083e7fae581247e9a985dddac
zxczxczczz <justwidenme@gmail.com>
2025-06-06 17:31:20 +0900

    feat: 기본 alert 템플릿 구조 추가

---
+++ front_end/src/components/AlertOverlay.vue
@@ -0,0 +1,8 @@
+<template>
+    <div v-if="isActive" class="alert-overlay" :class="{ 'blinking': isBlinking }">
+      <div class="alert-content">
+        <div class="alert-icon">⚠️</div>
+        <div class="alert-message">{{ message }}</div>
+      </div>
+    </div>
+  </template>
